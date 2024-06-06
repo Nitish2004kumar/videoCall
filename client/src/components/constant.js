694b1ffc4363ff95e2eb18b9e814bed29c368d50
Nitish2004kumar <nitishkumar2004lps@gmail.com>
2024-06-06 18:15:57 +0530

    api key is updated

--- client/src/components/constant.js
+++ client/src/components/constant.js
@@ -1,3 +1,3 @@
-export const App_ID=1997694484;
-export const SERVER_SECRET="1cfb0d17dba7fb9462923a83658ad079";
+export const App_ID=1939573190;
+export const SERVER_SECRET="06370590dcc4fd75e437519e061ac8d8";
 export const PASSWORD="nitish123";
